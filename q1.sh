ffced2e798da121b07271cd4687b7035f1d53973
AkashdeepKar <akashdeep.kar@niser.ac.in>
2019-08-09 20:52:52 +0530

    week1 assignment done

---
+++ q1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "please enter your name: "
+read name
+mkdir $name
+cd $name
+touch $name.cpp
+ls
---
+++ q2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Please enter your name: "
+read name1
+mkdir $name1
+cd $name1
+touch $name1-1.cpp
+touch $name1-2.cpp
+touch $name1-3.cpp
+touch $name1-4.cpp
+touch $name1-5.cpp
+ls
